ab7bad0d5da86cc4771bd4626403174d6b223e52
KariminK <103779342+KariminK@users.noreply.github.com>
2024-10-14 15:41:22 +0200

    init commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { exec } from "child_process";
+import express, { RequestHandler } from "express";
+
+const app = express();
+const port = 3000;
+
+const playVideo: RequestHandler = (req, res) => {
+  const { videoId } = req.params;
+  exec(`mpv https://youtu.be/${videoId}`, (err, stdout) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send();
+    }
+  });
+};
+
+app.get("/:videoId", playVideo);
+
+app.listen(port, () => {
+  console.log("SERVER LISTENING! PORT: 3000");
+});
